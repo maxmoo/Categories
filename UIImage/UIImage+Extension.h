b251cf6fc105e6c9ef91d14b3b8bb1def5611712
maxmoo <maxmoo@163.com>
2016-03-21 17:06:21 +0800

    maxmoo

---
+++ UIImage/UIImage+Extension.h
@@ -0,0 +1,16 @@
+//
+//  UIImage+Extension.h
+//  CCCategories
+//
+//  Created by maxmoo on 16/3/21.
+//  Copyright © 2016年 maxmoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Extension)
+
++(UIImage *)imageWithColor:(UIColor *)aColor;
++(UIImage *)imageWithColor:(UIColor *)aColor withFrame:(CGRect)aFrame;
+
+@end
